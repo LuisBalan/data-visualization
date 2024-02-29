a337cb6b2cf2f9320dd73420f70d17a4709f8dbf
LuisBalan <jlbalcz@gmail.com>
2024-02-29 01:37:55 -0600

    invoke pie chart into dashboard

--- src/app/main-dashboard/main-dashboard.component.ts
+++ src/app/main-dashboard/main-dashboard.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { LineChartComponent } from '../line-chart/line-chart.component';
+import { PieChartComponent } from '../pie-chart/pie-chart.component';
+
 
 @Component({
   selector: 'app-main-dashboard',
   standalone: true,
-  imports: [LineChartComponent],
+  imports: [LineChartComponent, PieChartComponent],
   templateUrl: './main-dashboard.component.html',
   styleUrl: './main-dashboard.component.scss'
 })
