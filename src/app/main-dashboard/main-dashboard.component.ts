138037fa0bd605474d97dcf655c811ddc8f80d7f
LuisBalan <jlbalcz@gmail.com>
2024-02-29 02:47:50 -0600

    add pie and doughnut charts to dashboard

--- src/app/main-dashboard/main-dashboard.component.ts
+++ src/app/main-dashboard/main-dashboard.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { LineChartComponent } from '../line-chart/line-chart.component';
 import { PieChartComponent } from '../pie-chart/pie-chart.component';
 import { BasicChartComponent } from '../basic-chart/basic-chart.component';
+import { DoughnutChartComponent } from '../doughnut-chart/doughnut-chart.component';
 
 
 @Component({
   selector: 'app-main-dashboard',
   standalone: true,
-  imports: [LineChartComponent, PieChartComponent, BasicChartComponent ],
+  imports: [
+    LineChartComponent,
+    PieChartComponent,
+    BasicChartComponent,
+    DoughnutChartComponent
+  ],
   templateUrl: './main-dashboard.component.html',
   styleUrl: './main-dashboard.component.scss'
 })
