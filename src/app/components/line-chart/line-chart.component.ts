f7e156674ac2737df3a7dc30b429ffa6ac314886
LuisBalan <jlbalcz@gmail.com>
2024-03-01 02:59:01 -0600

    import LIneCharData interface into line-chart-component

--- src/app/components/line-chart/line-chart.component.ts
+++ src/app/components/line-chart/line-chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ChartModule } from 'primeng/chart';
-import { LineChartData } from '../../line-chart-data';
+import { LineChartData } from '../../interfaces';
 
 
 @Component({
@@ -22,25 +22,6 @@ export class LineChartComponent implements OnInit {
       const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
       const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
 
-    //   this.data = {
-    //       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    //       datasets: [
-    //           {
-    //               label: 'First Dataset',
-    //               data: [65, 59, 80, 81, 56, 55, 40],
-    //               fill: false,
-    //               borderColor: documentStyle.getPropertyValue('--blue-500'),
-    //               tension: 0.4
-    //           },
-    //           {
-    //               label: 'Second Dataset',
-    //               data: [28, 48, 40, 19, 86, 27, 90],
-    //               fill: false,
-    //               borderColor: documentStyle.getPropertyValue('--pink-500'),
-    //               tension: 0.4
-    //           }
-    //       ]
-    //   };
 
     this.data = this.dataForLineChart;
 
