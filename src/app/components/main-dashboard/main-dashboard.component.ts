32038e470eacb8891d4d8854838dc15770e1b19f
LuisBalan <jlbalcz@gmail.com>
2024-03-01 01:41:21 -0600

    pass info to app-line-chart in main dashboard

--- src/app/components/main-dashboard/main-dashboard.component.ts
+++ src/app/components/main-dashboard/main-dashboard.component.ts
@@ -24,4 +24,38 @@ import { CardModule } from 'primeng/card';
 })
 export class MainDashboardComponent {
 
+  data: any
+
+  ngOnInit() { 
+    const documentStyle = getComputedStyle(document.documentElement);
+
+    this.data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+          {
+              label: 'First Dataset',
+              data: [65, 59, 80, 81, 56, 55, 40],
+              fill: false,
+              borderColor: documentStyle.getPropertyValue('--blue-500'),
+              tension: 0.4
+          },
+          {
+              label: 'Second Dataset',
+              data: [28, 48, 40, 19, 86, 27, 90],
+              fill: false,
+              borderColor: documentStyle.getPropertyValue('--pink-500'),
+              tension: 0.4
+          },
+          {
+            label: 'Third Dataset',
+            data: [33, 53, 45, 24, 91, 32, 95],
+            fill: false,
+            borderColor: documentStyle.getPropertyValue('--red-500'),
+            tension: 0.4
+        }
+      ]
+  };
+}
+
+
 }
