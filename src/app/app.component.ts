f72df0939640b587c5e7b571e5b022de9bcdfa90
LuisBalan <jlbalcz@gmail.com>
2024-03-28 00:51:34 -0600

    changes in layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, isDevMode } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainDashboardComponent } from './components/main-dashboard/main-dashboard.component';
- 
+import { ToolbarModule } from 'primeng/toolbar';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainDashboardComponent],
+  imports: [RouterOutlet, MainDashboardComponent, ToolbarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
