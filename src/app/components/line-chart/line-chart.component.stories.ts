6e19bcf40928c8ca2d829ca21b1a6b9a08f6addf
Luis Balan <jlbalcz@gmail.com>
2024-06-26 03:02:16 -0500

    create basic storybook story for line-chart component

---
+++ src/app/components/line-chart/line-chart.component.stories.ts
@@ -0,0 +1,51 @@
+import { LineChartComponent } from "./line-chart.component";
+import { componentWrapperDecorator, type Meta, type StoryObj } from "@storybook/angular";
+
+type Story = StoryObj<LineChartComponent>;
+
+export const Primary: Story = {
+    args: {
+        // dataForLineChart = {
+        //     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August'],
+        //     datasets: [
+        //         {
+        //             label: 'Radio',
+        //             data: [65, 59, 80, 81, 56, 55, 40, 53],
+        //             fill: false,
+        //             borderColor: documentStyle.getPropertyValue('--blue-500'),
+        //             tension: 0.12
+        //         },
+        //         {
+        //             label: 'Estacionamiento',
+        //             data: [28, 48, 40, 19, 86, 27, 90, 18],
+        //             fill: false,
+        //             borderColor: documentStyle.getPropertyValue('--pink-500'),
+        //             tension: 0.05
+        //         },
+        //         {
+        //           label: 'Inmuebles',
+        //           data: [33, 53, 45, 24, 91, 32, 95, 23],
+        //           fill: false,
+        //           borderColor: documentStyle.getPropertyValue('--red-500'),
+        //           tension: 0.05
+        //       },
+        //       {
+        //         label: 'Publicidad',
+        //         data: [35, 55, 47, 45, 93, 97, 95, 25],
+        //         fill: false,
+        //         borderColor: documentStyle.getPropertyValue('--blue-500'),
+        //         tension: 0.05
+        //     }
+        //     ]
+        // }
+    }
+};
+
+const meta: Meta<LineChartComponent> = {
+    component: LineChartComponent,
+    decorators: [
+        componentWrapperDecorator((story) => `<div style="width:50%; height:540%">${story}</div>`)
+    ],
+};
+
+export default meta;
