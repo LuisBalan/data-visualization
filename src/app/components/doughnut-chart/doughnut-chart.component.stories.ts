519329283eb28e1f47818229d2fe41b96f9ca8e8
Luis Balan <jlbalcz@gmail.com>
2024-06-26 01:33:12 -0500

    create storybook story for doughnut chart

---
+++ src/app/components/doughnut-chart/doughnut-chart.component.stories.ts
@@ -0,0 +1,20 @@
+import { DoughnutChartComponent } from "./doughnut-chart.component";
+import { componentWrapperDecorator, moduleMetadata, type Meta, type StoryObj } from "@storybook/angular/"
+
+type Story = StoryObj<DoughnutChartComponent>;
+
+export const Primary: Story = {
+    args: {
+        
+    }
+};
+
+const meta: Meta<DoughnutChartComponent> = {
+    component: DoughnutChartComponent,
+    decorators: [
+        componentWrapperDecorator((story) => `<div style="width: 40%; height: 40%">${story}</div>`),
+    ],
+};
+export default meta;
+
+
