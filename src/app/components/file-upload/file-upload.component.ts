7c12a7af2d4277acabf76db7c12a1fa044ba2aa4
Luis Balan <jlbalcz@gmail.com>
2024-07-05 01:26:55 -0500

    set component to receive csv file

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { Message, MessageService } from 'primeng/api';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ToastModule } from 'primeng/toast';
+import { CommonModule } from '@angular/common';
+
+interface UploadEvent {
+  originalEvent: Event;
+  files: File[];
+};
 
 @Component({
   selector: 'app-file-upload',
   standalone: true,
-  imports: [],
+  imports: [ FileUploadModule, ToastModule, CommonModule],
   templateUrl: './file-upload.component.html',
-  styleUrl: './file-upload.component.scss'
+  styleUrl: './file-upload.component.scss',
+  providers: [MessageService]
 })
 export class FileUploadComponent {
+
+  uploadedFiles: any[] = [];
+
+  constructor(private messageService: MessageService){};
+
+  onUpload(event: any){
+    for(let file of event.files){
+      this.uploadedFiles.push(file);
+    };
+    this.messageService.add({
+      severity: 'info',
+      summary: 'File Uploaded',
+      detail: ''
+    });
+  };
 
 }
