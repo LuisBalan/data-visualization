d6782e6f708e5ea4776180770360b55887414adf
Luis Balan <jlbalcz@gmail.com>
2024-07-16 03:47:08 -0500

    changes to improve file uploader

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, viewChild } from '@angular/core';
 import { Message, MessageService } from 'primeng/api';
 import { FileUploadModule } from 'primeng/fileupload';
 import { ToastModule } from 'primeng/toast';
@@ -18,20 +18,62 @@ interface UploadEvent {
   providers: [MessageService]
 })
 export class FileUploadComponent {
-
+  @ViewChild('uploadedFile') uploadedFile!: any;
+  @ViewChild('csvUploader') csvUploader!: any;
+  fileContent!: any;
+  tableTitles: string[] = [];
+ 
   uploadedFiles: any[] = [];
-
+  
   constructor(private messageService: MessageService){};
 
+
   onUpload(event: any){
-    for(let file of event.files){
-      this.uploadedFiles.push(file);
+    console.log('--- even target file --- ', event.currentFiles[0])
+    this.fileContent = event.currentFiles[0];
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      console.log(fileReader.result)
+      console.log('file reader: ', fileReader.readAsText(this.fileContent))
+      let arrayResult = fileReader.result?.toString().split('\n');
+      console.log('--- array result ---', arrayResult)
+  
     };
-    this.messageService.add({
-      severity: 'info',
-      summary: 'File Uploaded',
-      detail: ''
-    });
+    // for(let file of event.files){
+    //   this.uploadedFiles.push(file);
+    // };
+    // this.messageService.add({
+    //   severity: 'info',
+    //   summary: 'File Uploaded',
+    //   detail: ''
+    // });
+  };
+  
+  seeContent(event: any) {
+    let fileReader = new FileReader();
+    console.log('event: file', event.file)
+    console.log('file: ', this.uploadedFile)
+    console.log('csv uploader: ', this.csvUploader)
+    console.log('---', this.csvUploader.event)
+    console.log('--->', event.target.files[0]);
+    this.fileContent = event.target.files[0];
+    fileReader.onload = () => {
+      console.log(fileReader.result)
+      let csvRows = (fileReader.result as string).split('\n');
+      // this.tableTitles = csvRows[0];
+      console.log('header: ', this.tableTitles)
+
+    };
+    console.log('read as a text: ', fileReader.readAsText(this.fileContent))
+  };
+
+  showContent(){
+    let fileReader = new FileReader();
+
+    fileReader.onload = (e) => {
+      console.log('file reader: ', fileReader.readAsArrayBuffer(this.fileContent))
+    };
+
   };
 
 }
