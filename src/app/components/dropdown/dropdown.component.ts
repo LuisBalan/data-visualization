e7ac1c5f4e8717b4b5fa99ba298999b00e754456
Luis Balan <jlbalcz@gmail.com>
2024-07-26 01:19:47 -0500

    changes to join checkboxes and select in an isolated component

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -1,12 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { DropdownModule } from 'primeng/dropdown';
+import { CheckboxModule } from 'primeng/checkbox';
+import { Form, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dropdown',
   standalone: true,
-  imports: [],
+  imports: [DropdownModule, CheckboxModule, FormsModule, ReactiveFormsModule, CommonModule, JsonPipe],
   templateUrl: './dropdown.component.html',
   styleUrl: './dropdown.component.scss'
 })
 export class DropdownComponent {
+
+  @Input() column!: any;
+
+  formGroup!: FormGroup;
+  selectControl!: FormControl
+  selectForm!: any;
+  // reactiveDropDown!: FormControl
+  // reactiveCheckbox!: FormControl;
+  checked = true;
+  columnForm!: any;
+
+  options: any = [
+    {label: 'Número entero', value: 'int'},
+    {label: 'Decimal', value: 'float64'},
+    {label: 'Fecha', value: 'date'},
+    {label: 'Texto', value: 'string'},
+  ];
+
+  checkBoxOption: any = {label: 'Columna 1', value: 'col1'}
+
+  selectValue!: any;
+
+  constructor( private fb: FormBuilder){
+    this.columnForm = this.fb.group({
+      reactiveDropDown: new FormControl('string'),
+      reactiveCheckbox: new FormControl(true)
+    })
+    
+  };
+
+  ngOnInit() {
+  this.selectControl=new FormControl<any>('')
+  this.selectValue = document.getElementById('select')
+  this.selectForm = document.getElementById('selectForm')
+  this.columnForm.controls['reactiveDropDown'].setValue('');
+
+  this.columnForm.controls['reactiveDropDown'].valueChanges.subscribe((value: any)=> console.log('value changes: ', value))
+  this.columnForm.value.valueChanges((value: any) => {console.log('columns Form value: ', value)})
+}
+
+
+selectChange(event: any) {console.log(event)
+  console.log('select value: ', this.selectValue.value)
+  let selectedValue = event.target.value
+  console.log('event: ', selectedValue)
+  this.assignValueToSelect(event.target.value);
+
+}
+
+assignValueToSelect(arg: any){
+  const valuesOptions = this.options.map((option: any) => option.label);
+  const labelOptions = this.options.map((option: any) => option.value)
+  console.log('value options: ', valuesOptions)
+  let targetOption = valuesOptions.indexOf(arg)
+  let selectValue: string = labelOptions[targetOption];
+  this.columnForm.controls['reactiveDropDown'].patchValue(selectValue)
+}
+
+
 
 }
